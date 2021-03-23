59e17f8638bff1f13a72ad1401d410d8b1cc76a5
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2021-03-23 13:06:26 +0000

    Protein translation

--- protein-translation/protein_translation.go
+++ protein-translation/protein_translation.go
@@ -1,1 +1,47 @@
 package protein
+
+import "errors"
+
+var ErrStop error = errors.New("ErrStop")
+var ErrInvalidBase error = errors.New("ErrInvalidBase")
+
+func FromCodon(codon string) (string, error) {
+	switch codon {
+	case "AUG":
+		return "Methionine", nil
+	case "UUU", "UUC":
+		return "Phenylalanine", nil
+	case "UUA", "UUG":
+		return "Leucine", nil
+	case "UCU", "UCC", "UCA", "UCG":
+		return "Serine", nil
+	case "UAU", "UAC":
+		return "Tyrosine", nil
+	case "UGU", "UGC":
+		return "Cysteine", nil
+	case "UGG":
+		return "Tryptophan", nil
+	case "UAA", "UAG", "UGA":
+		return "", ErrStop
+	default:
+		return "", ErrInvalidBase
+	}
+}
+
+func FromRNA(rna string) ([]string, error) {
+	if rna == "" {
+		return []string{}, nil
+	}
+
+	protein, err := FromCodon(rna[:3])
+	if err != nil {
+		return nil, err
+	}
+
+	proteins, err := FromRNA(rna[3:])
+	if err == ErrStop {
+		err = nil
+	}
+
+	return append([]string{protein}, proteins...), err
+}
