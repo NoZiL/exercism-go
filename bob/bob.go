9820c2d1f541bc65760f34756d7017fe2323b257
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2021-03-28 14:47:14 +0000

    Add bobb solution

--- bob/bob.go
+++ bob/bob.go
@@ -1,5 +1,26 @@
 package bob
 
+import "strings"
+
 func Hey(remark string) string {
-	return ""
+	remark = strings.Trim(remark, "\n\r \t")
+	shouting := isShouting(remark)
+	asking := strings.HasSuffix(remark, "?")
+	switch {
+	case shouting && asking:
+		return "Calm down, I know what I'm doing!"
+	case shouting:
+		return "Whoa, chill out!"
+	case asking:
+		return "Sure."
+	case remark == "":
+		return "Fine. Be that way!"
+	default:
+		return "Whatever."
+	}
+}
+
+func isShouting(str string) bool {
+	upper := strings.ToUpper(str)
+	return str == upper && upper != strings.ToLower(str)
 }
