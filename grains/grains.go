1af05655ed1c5e42aee86789736f161a8dad5ce9
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2021-06-08 09:33:11 +0000

    Optimize grains

--- grains/grains.go
+++ grains/grains.go
@@ -2,25 +2,30 @@ package grains
 
 import "errors"
 
+const maxSquares = 64
+
+// Square returns how many grains were on a given square
 func Square(n int) (uint64, error) {
 	switch {
 	case n < 1:
 		return 0, errors.New("must be positive number")
-	case n > 64:
+	case n > maxSquares:
 		return 0, errors.New("must be inferior than or equal to 64")
-	case n == 1:
-		return 1, nil
 	}
-
-	t, _ := Square(n - 1)
-	return t * 2, nil
+	acc := uint64(1)
+	for i := 1; i < n; i++ {
+		acc *= 2
+	}
+	return acc, nil
 }
 
+// Total returns the total number of grains on the chessboard
 func Total() uint64 {
-	var sum uint64
-	for i := 1; i <= 64; i++ {
-		sq, _ := Square(i)
-		sum += sq
+	acc := uint64(1)
+	sum := uint64(1)
+	for i := 1; i <= maxSquares; i++ {
+		acc *= 2
+		sum += acc
 	}
 	return sum
 }
