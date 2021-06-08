a8d04566fd82600c51f060cb0ebfba27cd3b411a
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2021-06-08 12:03:52 +0000

    Optimise grains

--- grains/grains.go
+++ grains/grains.go
@@ -6,11 +6,8 @@ const maxSquares = 64
 
 // Square returns how many grains were on a given square
 func Square(n int) (uint64, error) {
-	switch {
-	case n < 1:
-		return 0, errors.New("must be positive number")
-	case n > maxSquares:
-		return 0, errors.New("must be inferior than or equal to 64")
+	if n < 1 || n > maxSquares {
+		return 0, errors.New("n must be number between 1 and 64")
 	}
 	return 1 << (n - 1), nil
 }
